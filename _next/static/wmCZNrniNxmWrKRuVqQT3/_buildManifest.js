070a074a696f6b7e7521698fe2aefbf159b17e83
Javeria Nigar <nigarhaider86@gmail.com>
2026-01-10 23:57:27 +0500

    Updates

--- _next/static/wmCZNrniNxmWrKRuVqQT3/_buildManifest.js
+++ _next/static/wmCZNrniNxmWrKRuVqQT3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Hackathon-2_phase-II//_next/:path+",
-        "destination": "/Hackathon-2_phase-II/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
